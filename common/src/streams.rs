46d881dd4683d140d0747565b97a976db5f18172
Thomas Havlik <thavlik@protonmail.com>
2026-02-05 17:20:19 -0500

    ...

--- common/src/streams.rs
+++ common/src/streams.rs
@@ -2,8 +2,14 @@ use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 pub mod topics {
+    use uuid::Uuid;
+
     pub const CACHE_MISS: &str = "histion.cache.miss";
     pub const PROCESS_SLIDE: &str = "histion.process.slide";
+
+    fn tile_data(id: Uuid) -> String {
+        format!("histion.tile.{}", id)
+    }
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
