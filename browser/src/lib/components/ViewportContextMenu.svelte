1fc79af0c5c394c13b03339a2c5e3add704ae389
Thomas Havlik <thavlik@protonmail.com>
2026-02-08 21:44:34 -0500

    ...

--- browser/src/lib/components/ViewportContextMenu.svelte
+++ browser/src/lib/components/ViewportContextMenu.svelte
@@ -255,8 +255,8 @@
       title={!isLoggedIn ? 'Log in to use ROI analysis' : ''}
     >
       <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-        <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
-        <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
+        <circle cx="11" cy="11" r="8"></circle>
+        <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
       </svg>
       Analyze Region
     </button>
