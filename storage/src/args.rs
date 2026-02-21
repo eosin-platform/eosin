7194836558ad47a28cca874d7122355f3f65ce80
Tom Havlik <thavlik@protonmail.com>
2026-02-21 11:33:53 -0500

    docs

--- storage/src/args.rs
+++ storage/src/args.rs
@@ -17,18 +17,27 @@ pub enum Commands {
 
 #[derive(Parser, Debug, Clone)]
 pub struct ServerArgs {
+    /// gRPC port to listen on for API requests
     #[arg(long, env = "API_PORT", default_value_t = 3500)]
     pub api_port: u16,
 
+    /// Root directory for storing data files
     #[arg(long, env = "DATA_ROOT", default_value = "/var/eosin")]
     pub data_root: String,
 
+    /// Port for replica sync communication (between nodes)
     #[arg(long, env = "CLUSTER_PORT")]
     pub cluster_port: Option<u16>,
 
+    /// Port for control plane communication (from the operator, e.g. topology changes)
+    #[arg(long, env = "CONTROL_PORT")]
+    pub control_port: Option<u16>,
+
+    /// Address of the master node to connect to (for replicas)
     #[arg(long, env = "MASTER")]
     pub master: Option<SocketAddr>,
 
+    /// Shard ID for this node (if using sharding)
     #[arg(long, env = "SHARD")]
     pub shard: Option<u32>,
 
