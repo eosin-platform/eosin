02b47f9c5c1836f921450327fd725c660431f338
Tom Havlik <thavlik@protonmail.com>
2026-03-18 23:32:37 -0400

    fix psql script

--- scripts/psql.sh
+++ scripts/psql.sh
@@ -14,6 +14,6 @@ export POSTGRES_PASSWORD=$(secret password)
 export POSTGRES_PORT=$(secret port)
 export POSTGRES_DB=$(secret database)
 export POSTGRES_SSL_MODE=$(secret sslmode)
-docker run -it --rm postgres:14 \
+docker run -it -v "$(pwd)":/scripts --rm postgres:14 \
     psql "host=$POSTGRES_HOST port=$POSTGRES_PORT user=$POSTGRES_USER password=$POSTGRES_PASSWORD dbname=$POSTGRES_DB sslmode=$POSTGRES_SSL_MODE" \
-    $@
+    "$@"
